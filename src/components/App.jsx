dc6efae36c4e8dfc242fbfd7afb4304369e70ffa
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-11-29 16:54:37 +0200

    one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,6 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
